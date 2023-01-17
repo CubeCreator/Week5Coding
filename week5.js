031c00b18875bf0bc1274cac04eb710e292e47e7
Simon Lucas <simonlucas@hotmail.ca>
2023-01-17 18:30:24 -0500

    finalizing

--- week5.js
+++ week5.js
@@ -8,12 +8,11 @@ class MenuObject {
 class Menu {
     constructor() {
         this.menulist = []
-        this.selectedObject = null;
     }
 
     start() {
         let selection = this.showMenuOptions();
-
+        //Selection on what event will trigger
         while (selection != 0) {
             switch (selection) {
                 case '1':
@@ -32,31 +31,34 @@ class Menu {
         }
         alert('Goodbye!')
     }
+    //Display interface and get input for the events
     showMenuOptions() {
         return prompt (' 0) Exit \n 1) Create New Menu Object \n 2) View Menu \n 3) Delete Menu Object');
     }
-
+//Code for Creating New Elements to the Array
     createMenuObject() {
         let menuName = prompt('Enter name for new menu option:')
         let menuLink = prompt('Enter link destination for menu option:')
         this.menulist.push(new MenuObject(menuName, menuLink))
     }
-
+//Code for Displaying the Entire Array
     viewMenuObject() {
         let menuDisplay = '';
         for (let i = 0; i < this.menulist.length; i++) {
+            console.log("index:",i,"menuDisplay:",menuDisplay);
             menuDisplay += i + ') ' + this.menulist[i].menuName + ' - ' + this.menulist[i].menuLink + '\n';
         }
         alert(menuDisplay)
     }
-
+//Code for Deleting Elements of the Array by Index Number
     deleteMenuObject() {
         let index = prompt('Enter the index of the menu option you wish to delete:');
-        if (index > -1 && index < this.selectedObject.menulist.length) {
-            this.selectedObject.menulist.splice(index, 1)
+        if (index > -1 && index < this.menulist.length) {
+            console.log("index:", index, "Selected Object:", this.menulist[index])
+            this.menulist.splice(index, 1)
         }
     }
 }
-
+//run the program on start
 let menu = new Menu();
 menu.start();
